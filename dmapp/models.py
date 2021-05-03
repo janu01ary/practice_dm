429b06e32ab51f488d34a7293381a1e779b33d4d
janu01ary <janu01ary@gmail.com>
2021-05-03 19:44:55 +0900

    user 외래키 연결, 보낸 쪽지들

--- dmapp/models.py
+++ dmapp/models.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 class Dm(models.Model):
-    sender_id = models.IntegerField() #발신자 id, user에서 아이디 외래키로 받아와야
-    receiver_id = models.IntegerField() #수신자 id, user에서 아이디 외래키로 받아와야
+    sender = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='sender', default='', null=True) #발신자 id, user에서 아이디 외래키로 받아와야
+    receiver = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='receiver', default='', null=True) #수신자 id, user에서 아이디 외래키로 받아와야
     content = models.CharField(max_length=500) #내용
     send_time = models.DateTimeField() #보낸 시간
 
--- dmapp/views.py
+++ dmapp/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
+from django.contrib.auth.models import User
 from .models import Dm
 
 # Create your views here.
 def home(request):
-    dms = Dm.objects.filter(receiver_id=request.user.id)
+    dms = Dm.objects.filter(receiver=request.user)
     return render(request, 'home.html', {'count': dms.count()})
 
 def dm_list(request):
-    dms = Dm.objects.filter(receiver_id=request.user.id)
-    return render(request, 'list.html', {'dms': dms})
+    dms = Dm.objects.filter(receiver=request.user)
+    send_dms = Dm.objects.filter(sender=request.user)
+    return render(request, 'list.html', {'dms': dms, 'send_dms': send_dms})
 
 def create(request):
     if request.method == 'POST':
         dm = Dm()
-        dm.sender_id = request.user.id
-        dm.receiver_id = request.POST['user_id']
+        dm.sender = request.user
+        dm.receiver = get_object_or_404(User, id=request.POST['user_id'])
         dm.content = request.POST['content']
         dm.send_time = timezone.datetime.now()
         dm.save()
-        return redirect('/detail/', str(dm.id))
+        return redirect('/list/')
     else:
         user_id = request.GET['sender_id']
         return render(request, 'create.html', {'user_id': user_id})
